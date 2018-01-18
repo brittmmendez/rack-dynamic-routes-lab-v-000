ca65c78062cf9ad426879d71639fc130d4bf1360
Brittany Morris <brittmmendez@gmail.com>
2018-01-18 16:00:56 +0000

    automatically backed up by learn

---
+++ app/application.rb
@@ -0,0 +1,20 @@
+class Application
+
+  resp=Rack::Response.new
+  req=Rack::Request.new(env)
+
+  if req.path.match(/items/)
+    item_name=req.path.split("/items/").last
+
+  else
+    resp.write "Route not found"
+    resp.status=404
+  end
+end
+    
+
+    
+
+    
+
+  
