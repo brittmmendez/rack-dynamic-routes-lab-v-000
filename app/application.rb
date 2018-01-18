8a82eb87dbdc1e6ee466120bafbc7c3bec7b6fd4
Brittany Morris <brittmmendez@gmail.com>
2018-01-18 16:11:49 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -5,10 +5,10 @@ class Application
     req=Rack::Request.new(env)
 
     if req.path.match(/items/)
-      #item_name=req.path.split("/items/").last
-      #item=@@items.find {|i| i.name==item_name}
+      item_name=req.path.split("/items/").last
+      item=@@items.find {|i| i.name==item_name}
 
-      resp.write "hello"#"#{item.price}"
+      resp.write "#{item.price}"
 
     else
       resp.write "Route not found"
