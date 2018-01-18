8b2bd3b9339c9991563bde29bac071edf2208af2
Brittany Morris <brittmmendez@gmail.com>
2018-01-18 16:14:28 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,7 @@ class Application
     resp=Rack::Response.new
     req=Rack::Request.new(env)
 
-    if req.path.match(/items/)
+    if req.path=="/items"
       item_name=req.path.split("/items/").last
       item=@@item.find {|i| i.name==item_name}
 
